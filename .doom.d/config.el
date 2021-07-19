0f347df43780655e4df855ad227e954f382b1c07
Daigo Kawasaki <emanon.was@gmail.com>
2021-07-19 12:39:10 +0900

    fix doom config : after ranger

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -53,6 +53,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq ranger-show-hidden 'format)
-(setq ranger-listing-dir-first nil)
-(define-key ranger-normal-mode-map (kbd "+") #'dired-create-directory)
+(after! ranger
+  (setq ranger-show-hidden 'format)
+  (setq ranger-listing-dir-first nil)
+  (define-key ranger-normal-mode-map (kbd "+") #'dired-create-directory))
